644c2bcdf052b64ee7d023e17e4bf8564cd9330e
Unknown <mjohnson030@west-mec.org>
2019-04-18 18:27:05 -0700

    Commit 1
    Created an onload function and created a for loop inside of the onload

---
+++ mpl_links.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 13
+   Case Problem 1
+
+   Author: MIllione Johnson
+   Date: 4/18/19  
+   
+   Filename: mpl_links.js
+
+*/
+
+window.addEventListener("load", function () {
+    var allSelect = select;
+    for (var i = 0; allSelect.length; i++) {
+
+    };
+});
